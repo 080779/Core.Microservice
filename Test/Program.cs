af84a7176534b07b556244b06d24fd8d0e5329fc
深渊 vz <exceed295@hotmail.com>
2018-08-22 17:17:33 +0800

    345543

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestTemplateCore;
+using System;
+using System.Net.Http;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                RestTemplate rest = new RestTemplate(httpClient);
+            }
+        }
+    }
+}
