78f94ed1f9ed37c4ca00e2d9406ac321fdf0dcec
深渊 vz <exceed295@hotmail.com>
2018-08-23 18:35:41 +0800

    34423

---
+++ Email/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Email.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Email")]
+    public class EmailController : Controller
+    {
+        public string Get()
+        {
+            return "发送网易邮箱成功";
+        }
+        public string Post()
+        {
+            return "发送腾讯邮箱成功";
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using RestTemplateCore;
+﻿using Polly;
+using RestTemplateCore;
 using System;
 using System.Net.Http;
 
@@ -8,10 +9,30 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            using (HttpClient httpClient = new HttpClient())
-            {
-                RestTemplate rest = new RestTemplate(httpClient);
-            }
+            /*
+            Policy policy = Policy.Handle<ArgumentException>().Fallback(()=> {
+                Console.WriteLine("执行出错");
+            }, ex => {
+                Console.WriteLine(ex);
+            });
+            policy.Execute(()=> {
+                Console.WriteLine("开始任务");
+                throw new ArgumentException("hello world!");
+                Console.WriteLine("完成任务");
+            });*/
+            Policy<string> policy = Policy<string>.Handle<ArgumentException>().Fallback(()=> {
+                Console.WriteLine("执行出错");
+                return "降级的值";
+            });
+
+            string res = policy.Execute(()=> {
+                Console.WriteLine("开始任务");
+                throw new ArgumentException("hello world!");
+                Console.WriteLine("完成任务");
+                return "正常的值";
+            });
+            Console.WriteLine($"返回值：{res}");
+            Console.ReadKey();
         }
     }
 }
